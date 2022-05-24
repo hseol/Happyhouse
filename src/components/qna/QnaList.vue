a16ffbb1aecd47bd6c728da9e0afedff3097b688
허설 <hseol96@naver.com>
2022-05-24 10:58:17 +0900

    add qna pagination and move to PaginatedList.vue

--- src/components/qna/QnaList.vue
+++ src/components/qna/QnaList.vue
@@ -1,50 +1,29 @@
 <template>
-  <b-container class="bv-example-row mt-3">
-    <b-row>
-      <b-col> </b-col>
-    </b-row>
-    <b-row class="mb-1">
-      <b-col class="text-right">
-        <b-button variant="outline-primary" @click="moveWrite()"
-          >글쓰기</b-button
-        >
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col v-if="articles.length">
-        <b-table-simple hover responsive>
-          <b-thead head-variant="dark">
-            <b-tr>
-              <b-th>글번호</b-th>
-              <b-th>제목</b-th>
-              <b-th>조회수</b-th>
-              <b-th>작성자</b-th>
-              <b-th>작성일</b-th>
-            </b-tr>
-          </b-thead>
-          <tbody>
-            <!-- 하위 component인 ListRow에 데이터 전달(props) -->
-            <qna-list-item
-              v-for="article in articles"
-              :key="article.articleno"
-              v-bind="article"
-            />
-          </tbody>
-        </b-table-simple>
-      </b-col>
+  <div>
+    <b-container class="bv-example-row mt-3">
+      <b-row class="mb-1">
+        <b-col class="text-right">
+          <b-button variant="outline-primary" @click="moveWrite()"
+            >글쓰기</b-button
+          >
+        </b-col>
+      </b-row>
+
+      <paginated-list :pageSize="2" :list-array="articles" />
       <!-- <b-col v-else class="text-center">도서 목록이 없습니다.</b-col> -->
-    </b-row>
-  </b-container>
+    </b-container>
+  </div>
 </template>
 
 <script>
 import { listArticle } from "@/api/qna.js";
-import QnaListItem from "@/components/qna/item/QnaListItem";
+import PaginatedList from "@/components/layout/PaginatedList";
+//import BoardListItem from "@/components/board/item/BoardListItem";
 
 export default {
-  name: "QnaList",
+  name: "qnaList",
   components: {
-    QnaListItem,
+    PaginatedList,
   },
   data() {
     return {
@@ -65,7 +44,7 @@ export default {
       },
       (error) => {
         console.log(error);
-      },
+      }
     );
   },
   methods: {
@@ -73,6 +52,7 @@ export default {
       this.$router.push({ name: "qnaRegister" });
     },
   },
+  computed: {},
 };
 </script>
 
